f88740c7588d1c38b42f96c773271866c333bede
alexgnatrow <lesha.gnat@gmail.com>
2019-02-08 20:03:45 +0200

    refactor to separate lazy_static Tera templates from server signature

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,72 @@
 #[macro_use]
 extern crate tera;
+#[macro_use]
+extern crate lazy_static;
 
-use actix_web;
-use actix_web::{error, fs, http, middleware, server, App, Error, HttpResponse, State};
+use actix_web::http::{header, Method, StatusCode};
+use actix_web::middleware::session::{self, RequestSession};
+use actix_web::{error, fs, http, middleware, pred, server, App, Error, HttpRequest, HttpResponse};
 use env_logger;
 use listenfd::ListenFd;
+use tera::{Context, Tera};
 mod router;
 
-struct AppState {
-    template: tera::Tera,
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = compile_templates!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/**/*"));
+        tera.autoescape_on(vec!["html", ".sql"]);
+        tera
+    };
 }
 
-fn index(state: State<AppState>) -> Result<HttpResponse, Error> {
-    let mut _ctx = tera::Context::new();
-    render_page(state, "pages/index.html")
+/// favicon handler
+fn favicon(req: &HttpRequest) -> Result<fs::NamedFile, Error> {
+    Ok(fs::NamedFile::open("assets/favicon.ico")?)
 }
 
-fn detail(state: State<AppState>) -> Result<HttpResponse, Error> {
-    let mut _ctx = tera::Context::new();
+///main page handler
+fn index(req: &HttpRequest) -> Result<HttpResponse, Error> {
+    let mut counter = 1;
+    if let Some(count) = req.session().get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        counter = count + 1;
+    }
 
-    render_page(state, "pages/detail.html")
+    // set counter to session
+    req.session().set("counter", counter)?;
+
+    let mut ctx = Context::new();
+    ctx.insert("counter", &counter);
+    render_with_ctx("pages/index.html", ctx)
 }
-fn calculate(state: State<AppState>) -> Result<HttpResponse, Error> {
-    let mut ctx = tera::Context::new();
+
+fn detail(req: &HttpRequest) -> Result<HttpResponse, Error> {
+    let mut _ctx = Context::new();
+
+    render_page("pages/detail.html")
+}
+fn calculate(req: &HttpRequest) -> Result<HttpResponse, Error> {
+    let mut ctx = Context::new();
     let calculated: u32 = 10 + 9876;
-    ctx.insert("calculated", &calculated.to_owned());
-    render_with_ctx(state, "pages/calculate.html", ctx)
+    ctx.insert("calculated", &calculated);
+    render_with_ctx("pages/calculate.html", ctx)
 }
 
-fn p404(state: State<AppState>) -> Result<HttpResponse, Error> {
-    let mut _ctx = tera::Context::new();
-    render_page(state, "pages/404.html")
+fn p404(req: &HttpRequest) -> Result<HttpResponse, Error> {
+    let mut _ctx = Context::new();
+    render_page("pages/404.html")
 }
 
-fn render_with_ctx(
-    state: State<AppState>,
-    template: &str,
-    ctx: tera::Context,
-) -> Result<HttpResponse, Error> {
-    let s = state
-        .template
+fn render_with_ctx(template: &str, ctx: Context) -> Result<HttpResponse, Error> {
+    let s = TEMPLATES
         .render(template, &ctx.to_owned())
         .map_err(|_| error::ErrorInternalServerError("Check template paths"))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-fn render_page(state: State<AppState>, template: &str) -> Result<HttpResponse, Error> {
-    let s = state
-        .template
-        .render(template, &tera::Context::new())
+fn render_page(template: &str) -> Result<HttpResponse, Error> {
+    let s = TEMPLATES
+        .render(template, &Context::new())
         .map_err(|_| error::ErrorInternalServerError("Check template paths"))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
@@ -57,21 +75,40 @@ fn main() {
     //init logger
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    //init autoreload
+
+    //init autoreload additional sockets
     let mut listenfd = ListenFd::from_env();
 
     let mut server = server::new(|| {
-        let tera = compile_templates!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/**/*"));
-
-        App::with_state(AppState { template: tera })
+        App::new()
             .middleware(middleware::Logger::default())
+            .middleware(session::SessionStorage::new(
+                session::CookieSessionBackend::signed(&[0; 32]).secure(false),
+            ))
             .handler("/static", fs::StaticFiles::new("static").unwrap())
-            .resource("/", |r| r.method(http::Method::GET).with(index))
-            .resource("/detail", |r| r.method(http::Method::GET).with(detail))
-            .resource("/calculate", |r| {
-                r.method(http::Method::GET).with(calculate)
+            .resource("/favicon", |r| r.get().f(favicon))
+            .resource("/", |r| r.get().f(index))
+            .resource("/detail", |r| r.get().f(detail))
+            .resource("/calculate", |r| r.get().f(calculate))
+            // redirect
+            .resource("/test", |r| {
+                r.get().f(|req| {
+                    println!("{:?}", req);
+                    HttpResponse::Found()
+                        .header(header::LOCATION, "pages/index.html")
+                        .finish()
+                })
             })
-            .default_resource(|r| r.method(http::Method::GET).with(p404))
+            // default
+            .default_resource(|r| {
+                // 404 for GET request
+                r.method(Method::GET).f(p404);
+
+                // all requests that are not `GET`
+                r.route()
+                    .filter(pred::Not(pred::Get()))
+                    .f(|_| HttpResponse::MethodNotAllowed());
+            })
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
--- src/router.rs
+++ src/router.rs
@@ -1,47 +1,47 @@
-// use actix_web::{error, Error, HttpResponse, State};
+use actix_web::{error, Error, HttpResponse, State};
 
-// pub struct AppState {
-//     template: tera::Tera,
-// }
+pub struct AppState {
+    template: tera::Tera,
+}
 
-// pub fn index(state: State<AppState>) -> Result<HttpResponse, Error> {
-//     let mut _ctx = tera::Context::new();
-//     render_page(state, "pages/index.html")
-// }
+pub fn index(state: State<AppState>) -> Result<HttpResponse, Error> {
+    let mut _ctx = tera::Context::new();
+    render_page(state, "pages/index.html")
+}
 
-// pub fn detail(state: State<AppState>) -> Result<HttpResponse, Error> {
-//     let mut _ctx = tera::Context::new();
+pub fn detail(state: State<AppState>) -> Result<HttpResponse, Error> {
+    let mut _ctx = tera::Context::new();
 
-//     render_page(state, "pages/detail.html")
-// }
-// pub fn calculate(state: State<AppState>) -> Result<HttpResponse, Error> {
-//     let mut ctx = tera::Context::new();
-//     let calculated: u32 = 10 + 9876;
-//     ctx.insert("calculated", &calculated.to_owned());
-//     render_with_ctx(state, "pages/calculate.html", ctx)
-// }
+    render_page(state, "pages/detail.html")
+}
+pub fn calculate(state: State<AppState>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    let calculated: u32 = 10 + 9876;
+    ctx.insert("calculated", &calculated.to_owned());
+    render_with_ctx(state, "pages/calculate.html", ctx)
+}
 
-// pub fn p404(state: State<AppState>) -> Result<HttpResponse, Error> {
-//     let mut _ctx = tera::Context::new();
-//     render_page(state, "pages/404.html")
-// }
+pub fn p404(state: State<AppState>) -> Result<HttpResponse, Error> {
+    let mut _ctx = tera::Context::new();
+    render_page(state, "pages/404.html")
+}
 
-// pub fn render_with_ctx(
-//     state: State<AppState>,
-//     template: &str,
-//     ctx: tera::Context,
-// ) -> Result<HttpResponse, Error> {
-//     let s = state
-//         .template
-//         .render(template, &ctx.to_owned())
-//         .map_err(|e| error::ErrorInternalServerError("Template error"))?;
-//     Ok(HttpResponse::Ok().content_type("text/html").body(s))
-// }
+pub fn render_with_ctx(
+    state: State<AppState>,
+    template: &str,
+    ctx: tera::Context,
+) -> Result<HttpResponse, Error> {
+    let s = state
+        .template
+        .render(template, &ctx.to_owned())
+        .map_err(|e| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
 
-// pub fn render_page(state: State<AppState>, template: &str) -> Result<HttpResponse, Error> {
-//     let s = state
-//         .template
-//         .render(template, &tera::Context::new())
-//         .map_err(|e| error::ErrorInternalServerError("Template error"))?;
-//     Ok(HttpResponse::Ok().content_type("text/html").body(s))
-// }
+pub fn render_page(state: State<AppState>, template: &str) -> Result<HttpResponse, Error> {
+    let s = state
+        .template
+        .render(template, &tera::Context::new())
+        .map_err(|e| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
